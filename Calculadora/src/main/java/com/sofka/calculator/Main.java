c99117a5df27c33476b6f40dd860fa092f80ae19
Ricardo Ortega <tattortega.28@gmail.com>
2022-05-25 16:33:13 -0500

    Se agrega la documentacion de la clase principal

--- Calculadora/src/main/java/com/sofka/calculator/Main.java
+++ Calculadora/src/main/java/com/sofka/calculator/Main.java
@@ -2,9 +2,17 @@ package com.sofka.calculator;
 
 import java.util.logging.Logger;
 
+/**
+ * Clase principal donde se ejecuta la calculadora
+ * Se crea una instancia de clase calculadora y se invocan los métodos para la operación seleccionada
+ * @author Mateo Gutierrez <mateog147@hotmail.com>
+ * @author Ricardo Ortega <tattortega.28@gmail.com>
+ * @version 1.0.0 2022/05/22
+ * @since 1.0.0
+ */
+
 public class Main {
     public static void main(String[] args) {
-
         try {
             Calculator calculator = new Calculator();
             calculator.inputNumber();
@@ -14,6 +22,5 @@ public class Main {
             Logger errorLogger = Logger.getLogger("errorLogger");
             errorLogger.warning("Error en la operación");
         }
-
     }
 }
