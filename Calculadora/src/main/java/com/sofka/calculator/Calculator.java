e117fd87985ce431ff9fb073d658674e5763c987
mateog147 <mateog147@hotmail.com>
2022-05-25 16:52:45 -0500

    Documentación clase calculadora, pendiente metodos

--- Calculadora/src/main/java/com/sofka/calculator/Calculator.java
+++ Calculadora/src/main/java/com/sofka/calculator/Calculator.java
@@ -4,12 +4,44 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Clase Calculadora.
+ *
+ * Contiene los metodos necesarios para efectuar sobre dos numeros las 4 operaciones basicas.
+ * - Suma.
+ * - Resta.
+ * - Multiplicación.
+ * - División.
+ *
+ * @author Mateo Gutierrez <mateog147@hotmail.com>
+ * @author Ricardo Ortega <tattortega.28@gmail.com>
+ * @version 1.0.0 2022/05/22
+ * @since 1.0.0
+ */
 public class Calculator {
+    /**
+     * Primer numero  de la operación, en caso de la division es el dividendo:
+     */
     private double numberA;
+    /**
+     * Segundo numero  de la operación, en caso de la division es el divisor.
+     */
     private double numberB;
+    /**
+     * Indentificador de la operación a ejecutar, entre 1 y 4.
+     */
     private int operator;
+    /**
+     * Resultado de efectuar la operación seleccionada para los dos numeros ingresados:
+     */
     private double result;
-    Logger logger = Logger.getLogger("logger");
+    /**
+     * Instancia de la clase loger, para registrar mensajes en consola
+     */
+    private final Logger logger = Logger.getLogger("logger");
+    /**
+     * Instancia de la clase Scanner para capturar valores por teclado.
+     */
     private final Scanner values;
 
     public Calculator() {
